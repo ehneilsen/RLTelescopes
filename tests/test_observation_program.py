baaf38a2a0444c47568df404768bab24f45e16a4
voetberg <magpie127@gmail.com>
2022-12-21 07:56:05 -0600

    tests

---
+++ tests/test_observation_program.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class TestObservationProgram(TestCase):
+    def test_init(self):
+        pass
+
+    def test_variables(self):
+        # Goes through and tests all the variables that have hard limits
+        pass
